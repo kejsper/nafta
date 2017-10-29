83244ebcece5099e4dcb5ceb559a34d3dbd9941c
kejsper\Programowanie <kejsper@gmail.com>
2017-10-29 13:08:37 +0100

    project setup

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const livereload = require('gulp-livereload');
